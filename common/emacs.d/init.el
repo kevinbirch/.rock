1592d4755a7a547367cbb03edf683eed7f1c8daa
kevin birch <kmb@pobox.com>
2012-11-15 22:05:25 -0500

    update default font to DejaVu Sans Mono

--- common/emacs.d/init.el
+++ common/emacs.d/init.el
@@ -471,4 +471,4 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 116 :width normal :family "Bitstream Vera Sans Mono")))))
+ '(default ((t (:stipple nil   :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 116 :width normal :family "DejaVu Sans Mono")))))
