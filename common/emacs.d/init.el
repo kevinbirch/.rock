958f44164717711e19cbcdb4371da44d9cbc87ce
kevin birch <kmb@percolate.com>
2013-05-07 13:41:33 -0400

    add ack mode

--- common/emacs.d/init.el
+++ common/emacs.d/init.el
@@ -19,6 +19,14 @@
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 (package-initialize)
 
+;; ack configuration
+(require 'ack-and-a-half)
+;; Create shorter aliases
+(defalias 'ack 'ack-and-a-half)
+(defalias 'ack-same 'ack-and-a-half-same)
+(defalias 'ack-find-file 'ack-and-a-half-find-file)
+(defalias 'ack-find-file-same 'ack-and-a-half-find-file-same)
+
 (require 'expand-region)
 (require 'eldoc)
 (require 'autopair)
