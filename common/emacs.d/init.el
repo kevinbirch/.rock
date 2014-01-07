b96b5026f196cb567e54a4fee57fb42259789e6c
kevin birch <kmb@pobox.com>
2014-01-07 17:44:06 -0500

    add flx-ido

--- common/emacs.d/init.el
+++ common/emacs.d/init.el
@@ -378,7 +378,12 @@
   )
 
 (require 'ido)
+(require 'flx-ido)
 (ido-mode t)
+(ido-everywhere 1)
+(flx-ido-mode 1)
+;; disable ido faces to see flx highlights.
+(setq ido-use-faces nil)
 
 ;; Add back in the ability to change case on regions
 (put 'upcase-region 'disabled nil)
