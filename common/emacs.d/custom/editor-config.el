6bd8476cc3059794429676b9d80f475cd047b3ef
kevin birch <kmb@pobox.com>
2014-10-11 15:41:30 -0400

    add recentf

--- common/emacs.d/custom/editor-config.el
+++ common/emacs.d/custom/editor-config.el
@@ -16,6 +16,12 @@
 (require 'auto-complete-config)
 (ac-config-default)
 
+;; recent files
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+
 (require 'expand-region)
 (require 'eldoc)
 (require 'autopair)
