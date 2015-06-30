05d92542e61c748914686ff3cf042024d3be5e5c
kevin birch <kmb@percolate.com>
2015-06-30 17:06:18 -0400

    remove ethan-wspace

--- common/emacs.d/custom/visual-config.el
+++ common/emacs.d/custom/visual-config.el
@@ -4,11 +4,6 @@
 (load-theme 'solarized-dark t)
 (set-default 'cursor-type 'box)
 
-;(require 'ethan-wspace)
-;(global-ethan-wspace-mode 1)
-;; ethan-wspace-mode doesn't work for makefiles, they require tabs
-;(add-hook 'makefile-mode-hook (lambda () (ethan-wspace-mode 0)))
-
 ;; Linum mode customizations
 (require 'linum+)
 (global-linum-mode 1)
