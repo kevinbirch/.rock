c2396403e344f6eee5acc245b0e7b300ffd8bf3c
kevin birch <kevin.birch@mavenclinic.com>
2019-07-18 17:35:33 -0400

    add highlight-indentation-current-column-mode to yaml mode

--- common/emacs.d/custom/yaml-mode-config.el
+++ common/emacs.d/custom/yaml-mode-config.el
@@ -1,3 +1,9 @@
 (require 'yaml-mode)
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
+
+(add-hook 'python-mode-hook
+          (lambda ()
+            (highlight-indentation-current-column-mode)
+            )
+          )
